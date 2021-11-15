4ca54578d011c28f6a15527706d0a88d4995c627
mzabovsky <michal.zabovsky@gmail.com>
2021-11-15 23:49:48 +0100

    Basic layout
    Basic objects placed on scene.

---
+++ Programming Theory/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+
+    private Color color;
+    private float speed;
+    private string infoText;
+
+
+    public Vehicle()
+    {
+
+    }
+
+}
