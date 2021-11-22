b965f7259b7eaf67626004265e3c9f2e8632e956
mzabovsky <michal.zabovsky@gmail.com>
2021-11-22 23:46:04 +0100

    Minor changes

--- Programming Theory/Assets/Scripts/Vehicle.cs
+++ Programming Theory/Assets/Scripts/Vehicle.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Vehicle : MonoBehaviour
+public abstract class Vehicle : MonoBehaviour
 {
 
     [SerializeField] private Material materialOn;
     [SerializeField] private Material materialOff;
     [SerializeField] private float speed;
     [SerializeField] private string infoText;
+    public TMP_Text infoTextHolder;
 
 
     public Vehicle()
     {
 
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+    public abstract void SwitchLight(bool turnOn);
+
+
+    public void ShowInfo()
+    {
+        infoTextHolder.SetText("Vehicle name: " + infoText);
     }
 
 }
